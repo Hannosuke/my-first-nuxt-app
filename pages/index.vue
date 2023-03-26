05bb7a34ac38a7e18b8625e53bc7d141a946bdea
ryuhanda <r.handa@everyleaf.com>
2023-03-26 15:17:36 +0900

    axios-moduleによる外部リソースの取得 yarn add @nuxt.js/axios

--- pages/index.vue
+++ pages/index.vue
@@ -23,11 +23,12 @@
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
+  async mounted() {
+    console.log(
+      JSON.stringify(await
+        this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt'), true, ' ')
+    )
   }
 }
 </script>
